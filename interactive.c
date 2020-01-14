23ef328d3e2b63416661362fb6ab7e3b2299c764
rajkumar patel <rajkumar.patel@seagate.com>
2020-01-14 09:27:10 +0530

    let's check what is happening

--- interactive.c
+++ interactive.c
@@ -1,11 +1,13 @@
+@@@@@@@@@@@
+
 first hunk
 
 #this should be one hunk
 
 one top level hunk
-
+$ $$$$$$$$$$$$$$$$
 #this would be seconde hung
 
-this is another hunk
-
-and this would be one more hunk
+   this is another hunk
+   ******************
+   and this would be one more hunk
